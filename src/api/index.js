16df12f9d1d43f0cb0b0046ea882c307c1a079de
Manjineshwaran <138910407+Manjineshwaran@users.noreply.github.com>
2025-04-26 00:39:13 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5555';
+const API_BASE_URL = 'https://convo-backend-fastapi-with-authentication.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
